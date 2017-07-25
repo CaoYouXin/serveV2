5b690c376a7c9ded68b64a1bae1f80c8b6cd68d9
曹力升 <c_youxin@163.com>
2017-07-25 21:18:40 +0800

    one call and return, perfect!lo

--- apis/blog/src/blog/controller/SetResourceLevelCtrl.java
+++ apis/blog/src/blog/controller/SetResourceLevelCtrl.java
@@ -2,6 +2,7 @@ package blog.controller;
 
 import auth.AuthHelper;
 import beans.BeanManager;
+import blog.data.EIResourceLevel;
 import blog.service.IResourceLevelService;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
@@ -12,7 +13,7 @@ import rest.WithMatcher;
 
 import java.io.IOException;
 
-public class ListResourceLevelCtrl extends WithMatcher {
+public class SetResourceLevelCtrl extends WithMatcher {
 
     private IResourceLevelService resourceLevelService = BeanManager.getInstance().getService(IResourceLevelService.class);
 
@@ -23,16 +24,21 @@ public class ListResourceLevelCtrl extends WithMatcher {
 
     @Override
     public String name() {
-        return "blog list resource level";
+        return "blog set resource level";
     }
 
     @Override
     public String urlPattern() {
-        return "/blog/resource-level/list";
+        return "/blog/resource-level/set";
     }
 
     @Override
     public void handle(HttpRequest httpRequest, HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
-        RestHelper.oneCallAndRet(httpResponse, this.resourceLevelService, "list");
+        if (!RestHelper.isPost(httpRequest, httpResponse)) {
+            return;
+        }
+
+        EIResourceLevel resourceLevel = RestHelper.getBodyAsObject(httpRequest, EIResourceLevel.class);
+        RestHelper.oneCallAndRet(httpResponse, this.resourceLevelService, "save", resourceLevel);
     }
 }
