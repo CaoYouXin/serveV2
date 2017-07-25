7e306992e2c29db80b678b860279b78f0e19f903
曹力升 <c_youxin@163.com>
2017-07-25 12:57:33 +0800

    nullable

--- apis/blog/src/blog/data/EIUserFavourRuleRec.java
+++ apis/blog/src/blog/data/EIUserFavourRuleRec.java
@@ -16,17 +16,17 @@ public interface EIUserFavourRuleRec extends EntityBeanI {
 
     void setUserFavourRuleRecId(Long userFavourRuleRecId);
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     Long getUserId();
 
     void setUserId(Long userId);
 
-    @Column(name = "rule_id")
+    @Column(name = "rule_id", nullable = false)
     Long getUserFavourRuleId();
 
     void setUserFavourRuleId(Long userFavourRuleId);
 
-    @Column(name = "count")
+    @Column(name = "count", nullable = false)
     Integer getRecCount();
 
     void setRecCount(Integer recCount);
