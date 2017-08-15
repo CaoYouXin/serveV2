99c37225499109075be6b3d5bd542a58642b63a3
曹力升 <c_youxin@163.com>
2017-08-15 18:22:48 +0800

    暂时废掉resource-mapping

--- apis/blog/src/blog/repository/IResourceLevelMappingRepo.java
+++ apis/blog/src/blog/repository/IResourceLevelMappingRepo.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface IResourceLevelMappingRepo extends Repository<EIResourceLevelMapping, Long> {
 
-    @Query(value = "select a.ResourceLevelId, a.ResourceLevelName, a.ResourceLevelExpMsg, b.ResourceUrlPrefix, b.ResourceLevelMappingId from EIResourceLevelMapping b, EIResourceLevel a where a.ResourceLevelId = b.ResourceLevelId and b.ResourceLevelMappingDisabled = 0", types = {EIResourceLevelMapping.class, EIResourceLevel.class})
+    @Query(value = "select a.ResourceLevelId, a.ResourceLevelName, a.ResourceLevelExpMsg, b.ResourceUrlPrefix, b.ResourceLevelMappingId, b.ResourceLevelMappingDisabled from EIResourceLevelMapping b, EIResourceLevel a where a.ResourceLevelId = b.ResourceLevelId", types = {EIResourceLevelMapping.class, EIResourceLevel.class})
     List<EIResourceLevelMappingDetail> queryAll();
 
     List<EIResourceLevelMapping> findAllByResourceLevelMappingDisabled(Boolean resourceLevelMappingDisabled);
