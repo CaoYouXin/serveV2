37618ab869ea529d6c97a20069ab543dc0b0a850
曹又心 <c_lisheng@163.com>
2017-09-16 19:15:45 +0800

    blogConsole. guard

--- html/blogConsole/src/app/app.router.ts
+++ html/blogConsole/src/app/app.router.ts
@@ -17,33 +17,34 @@ import {
   BlogCommentComponent
 } from "./component/index";
 import { TestComponent } from "./test/test.component";
+import { AuthGuard } from "./guard";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, data: { name: 'Home' } },
   {
-    path: 'resource', component: TabsComponent, data: { name: '资源管理' },
+    path: 'resource', component: TabsComponent, data: { name: '资源管理' }, canActivate: [AuthGuard],
     children: [
-      { path: 'types', component: ResourceManagerComponent, data: { name: "资源类型管理" } },
-      { path: 'mapping', component: ResourceMappingComponent, data: { name: "资源-文件映射管理" } },
-      { path: 'upload', component: ResourceUploadComponent, data: { name: "资源上传管理" } },
-      { path: 'deploy', component: ResourceDeployComponent, data: { name: "资源部署管理" } }
+      { path: 'types', component: ResourceManagerComponent, canActivate: [AuthGuard], data: { name: "资源类型管理" } },
+      { path: 'mapping', component: ResourceMappingComponent, canActivate: [AuthGuard], data: { name: "资源-文件映射管理" } },
+      { path: 'upload', component: ResourceUploadComponent, canActivate: [AuthGuard], data: { name: "资源上传管理" } },
+      { path: 'deploy', component: ResourceDeployComponent, canActivate: [AuthGuard], data: { name: "资源部署管理" } }
     ]
   },
-  { path: 'user', component: BlogUserComponent, data: { name: '用户管理' } },
+  { path: 'user', component: BlogUserComponent, canActivate: [AuthGuard], data: { name: '用户管理' } },
   {
-    path: 'favour', component: TabsComponent, data: { name: '好感度管理' },
+    path: 'favour', component: TabsComponent, data: { name: '好感度管理' }, canActivate: [AuthGuard],
     children: [
-      { path: 'list', component: FavourDetailComponent, data: { name: "用户好感度列表" } },
-      { path: 'mapping', component: FavourMappingComponent, data: { name: "好感度-资源映射管理" } },
-      { path: 'rule', component: FavourRuleComponent, data: { name: "好感度规则管理" } }
+      { path: 'list', component: FavourDetailComponent, canActivate: [AuthGuard], data: { name: "用户好感度列表" } },
+      { path: 'mapping', component: FavourMappingComponent, canActivate: [AuthGuard], data: { name: "好感度-资源映射管理" } },
+      { path: 'rule', component: FavourRuleComponent, canActivate: [AuthGuard], data: { name: "好感度规则管理" } }
     ]
   },
   {
-    path: 'post', component: TabsComponent, data: { name: 'POST 管理' },
+    path: 'post', component: TabsComponent, data: { name: 'POST 管理' }, canActivate: [AuthGuard],
     children: [
-      { path: 'category', component: BlogCategoryComponent, data: { name: "分类管理" } },
-      { path: 'post', component: BlogPostComponent, data: { name: "Post 管理" } },
-      { path: 'comment', component: BlogCommentComponent, data: { name: "评论管理" } }
+      { path: 'category', component: BlogCategoryComponent, canActivate: [AuthGuard], data: { name: "分类管理" } },
+      { path: 'post', component: BlogPostComponent, canActivate: [AuthGuard], data: { name: "Post 管理" } },
+      { path: 'comment', component: BlogCommentComponent, canActivate: [AuthGuard], data: { name: "评论管理" } }
     ]
   },
   { path: 'login', component: LoginComponent },
