080daf6c4c8e0819c9d09a36f75fbd9b6cf7507a
曹又心 <c_lisheng@163.com>
2017-10-01 17:05:25 +0800

    login decoration

--- html/albumConsole/src/app/user/login/login.component.ts
+++ html/albumConsole/src/app/user/login/login.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['../../commons/form.component.css', './login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  loading: boolean = false;
 
-  ngOnInit() {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private service: UserService,
+    private router: Router) {
   }
+
+  login(): void {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    this.loading = true;
+  }
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.loginForm = this.fb.group({
+      'username': [this.model.username, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(24)
+      ]],
+      'password': [this.model.password, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(24)
+      ]],
+    });
+
+    this.loginForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.loginForm) {
+      return;
+    }
+    const form = this.loginForm;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'username': '',
+    'password': ''
+  };
+
+  validationMessages = {
+    'username': {
+      'required': '用户名是必填项.',
+      'minlength': '用户名最短6个字符.',
+      'maxlength': '用户名最长24个字符.',
+    },
+    'password': {
+      'required': '密码是必填项.',
+      'minlength': '密码最短6个字符.',
+      'maxlength': '密码最长24个字符.',
+    }
+  };
 
 }
--- html/albumConsole/src/app/user/user.module.ts
+++ html/albumConsole/src/app/user/user.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from '../app.router';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   declarations: [LoginComponent]
 })
